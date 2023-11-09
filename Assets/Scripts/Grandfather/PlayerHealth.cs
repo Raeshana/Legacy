c55c2280368520acfa26bf721cb805381a73d228
YifeiP <yifeip@umich.edu>
2023-11-09 18:00:32 -0500

    added player hp bar

---
+++ Assets/Scripts/Grandfather/PlayerHealth.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+    public Slider healthBar;
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    private void Start() {
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
+
+    public void TakeDamage(int damage) {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth); // Ensure health doesn't go below 0 or above maxHealth
+        UpdateHealthBar();
+    }
+
+    void UpdateHealthBar() {
+        healthBar.value = (float)currentHealth / maxHealth;
+    }
+}
+
